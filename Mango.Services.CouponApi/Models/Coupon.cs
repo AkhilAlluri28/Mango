b882895a081283dbecd9d686b5d943420553b59d
Akhil Alluri <akhil.alluri28@gmail.com>
2024-02-13 11:24:11 -0600

    Initial set-up

---
+++ Mango.Services.CouponApi/Models/Coupon.cs
@@ -0,0 +1,11 @@
+﻿namespace Mango.Services.CouponApi.Models
+{
+    public record Coupon
+    {
+        public int CouponId { get; init; }
+        public string CouponCode { get; init; } = null!;
+
+        public decimal DiscountAmount { get; init; }
+        public decimal MinAmount { get; init; }
+    }
+}
---
+++ Mango.Services.CouponApi/Models/Dto/CouponDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Mango.Services.CouponApi.Models.Dto
+{
+    public record CouponDto
+    {
+        public int CouponId { get; init; }
+        public string CouponCode { get; init; } = null!;
+
+        public decimal DiscountAmount { get; init; }
+        public decimal MinAmount { get; init; }
+    }
+}
